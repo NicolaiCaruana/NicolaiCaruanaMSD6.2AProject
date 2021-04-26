300657c5a6f78112eb83a9a6f516fa5b61bcadbb
NicolaiCaruana <48438005+NicolaiCaruana@users.noreply.github.com>
2021-04-26 10:32:18 +0200

    Timer Added
    Next in line: button/onHover function

---
+++ Src/Scripts/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timer;
+    public float time;
+
+    float ms;
+    float sec;
+    float min;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        StartCoroutine("Stopwatch");
+    }
+
+    IEnumerator Stopwatch()
+    {
+        while (true)
+        {
+            time += Time.deltaTime;
+            ms = (int)((time - (int)time) * 100);
+            sec = (int)(time % 60);
+            min = (int)(time / 60 % 60);
+
+            timer.text = string.Format("{0:00}:{1:00}:{2:00}", min, sec, ms);
+
+            yield return null;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
